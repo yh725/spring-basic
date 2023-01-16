fda2550632c2f97c687ff419cbbf9907b44e649d
yh725 <yh725@kakao.com>
2023-01-16 23:46:22 +0900

    스프링 핵심 원리 이해1(예제 만들기) - 회원 도메인 실행과 테스트 - 주문과 할인 도메인 설계 - 주문과 할인 도메인 개발 - 주문과 할인 도메인 실행과 테스트

---
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,24 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceIml;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+	MemberService memberService = new MemberServiceIml();
+	OrderService orderService = new OrderServiceImpl();
+
+	@Test
+	void createOrder() {
+		Long memberId = 1L;
+		Member member = new Member(memberId, "memberA", Grade.VIP);
+		memberService.join(member);
+
+		Order order = orderService.createOrder(memberId, "iteamA", 10000);
+		Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+	}
+}
