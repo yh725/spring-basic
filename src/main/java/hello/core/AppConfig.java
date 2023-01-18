405ce7cb2e6d01ce91a67b7e9c7516922428afb9
yh725 <yh725@kakao.com>
2023-01-18 23:34:57 +0900

    스프링 핵심 원리 이해2(객체 지향 원리 적용) - AppConfig 리팩터링 - 새로운 구조와 할인 정책 적용 - 전체 흐름 정리 - 좋은 객체 지향 설계의 5가지 원칙의 적용 - IoC, DI, 그리고 컨테이너

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,9 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceIml;
 import hello.core.member.MemoryMemberRepository;
@@ -10,10 +13,20 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {
 
 	public MemberService memberService() {
-		return new MemberServiceIml(new MemoryMemberRepository());
+		return new MemberServiceIml(memberRepository());
 	}
 
 	public OrderService orderService() {
-		return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+		return new OrderServiceImpl(memberRepository(), discountPolicy());
 	}
+
+	public MemberRepository memberRepository() {
+		return new MemoryMemberRepository();
+	}
+
+	public DiscountPolicy discountPolicy() {
+//		return new FixDiscountPolicy();
+		return new RateDiscountPolicy();
+	}
+
 }
