0911bb867c11be7cfc31c261c5b94b9bea791984
yh725 <yh725@kakao.com>
2023-01-25 21:58:24 +0900

    의존관계 자동 주입 - 다양한 의존관계 주입 방법

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -12,14 +12,33 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService {
 
-	private final MemberRepository memberRepository;
-	private final DiscountPolicy discountPolicy;
+//	@Autowired
+	private final MemberRepository memberRepository; //
+
+//	@Autowired
+	private final DiscountPolicy discountPolicy; //
+
+//	@Autowired
+//	public void setMemberRepository(MemberRepository memberRepository) {
+//		this.memberRepository = memberRepository;
+//	}
+
+//	@Autowired
+//	public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//		this.discountPolicy = discountPolicy;
+//	}
 
 	@Autowired
 	public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
 		this.memberRepository = memberRepository;
 		this.discountPolicy = discountPolicy;
 	}
+
+//	@Autowired
+//	public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//		this.memberRepository = memberRepository;
+//		this.discountPolicy = discountPolicy;
+//	}
 
 	@Override
 	public Order createOrder(Long memberId, String itemName, int itemPrice) {
--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,9 +1,11 @@
 package hello.core.order;
 
 import hello.core.AppConfig;
+import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import hello.core.member.MemoryMemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -29,4 +31,14 @@ public class OrderServiceTest {
 		Order order = orderService.createOrder(memberId, "iteamA", 10000);
 		Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
 	}
+
+//	@Test
+//	void fieldInjectionTest() {
+//		OrderServiceImpl orderService = new OrderServiceImpl();
+
+//		orderService.setMemberRepository(new MemoryMemberRepository());
+//		orderService.setDiscountPolicy(new FixDiscountPolicy());
+
+//		orderService.createOrder(1L, "itemA", 10000);
+//	}
 }
